84a947e6d7ad68a4b8cef65e64e35755f0dbb156
Edward Sandor <ed@ewsandor.com>
2022-02-11 21:59:29 -0700

    Defining virtual motor driver for debug.

---
+++ sl_cr_motor_driver_virtual.cpp
@@ -0,0 +1,82 @@
+#include "string.h"
+#include "arduino.h"
+
+#include "sl_cr_motor_driver_virtual.hpp"
+
+void sl_cr_motor_driver_virtual_c::disable_motor()
+{
+  if(active)
+  {
+    char output_string[SL_CR_MOTOR_DRIVER_VIRTUAL_MAX_CHARS] = {0};
+    strlcpy(output_string, this->name, sizeof(output_string));
+    strlcat(output_string, " deactivated.", sizeof(output_string));
+    Serial.println(output_string);
+  }
+  active = false;
+}
+
+void sl_cr_motor_driver_virtual_c::command_motor()
+{
+  char output_string[SL_CR_MOTOR_DRIVER_VIRTUAL_MAX_CHARS] = {0};
+  char int_string[SL_CR_MOTOR_DRIVER_VIRTUAL_MAX_CHARS] = {0};
+
+  if(!active)
+  {
+    strlcat(output_string, " activated.", sizeof(output_string));
+    Serial.println(output_string);
+  }
+  active = true;
+
+  if(get_neutral_speed() == speed)
+  {
+    strlcpy(output_string, this->name, sizeof(output_string));
+    strlcat(output_string, " braking.", sizeof(output_string));
+    Serial.println(output_string);
+  }
+  else if (speed > get_neutral_speed())
+  {
+    /* Positive direction */
+    const int positive_range = get_max_speed()-get_neutral_speed();
+    int scaled_speed = ((speed-get_neutral_speed())*100)/positive_range;
+
+    strlcpy(output_string, this->name, sizeof(output_string));
+    strlcat(output_string, " speed ", sizeof(output_string));
+    itoa(scaled_speed, int_string, 10);
+    strlcat(output_string, int_string, sizeof(output_string));
+    strlcat(output_string, ". Raw speed ", sizeof(output_string));
+    itoa(speed, int_string, 10);
+    strlcat(output_string, int_string, sizeof(output_string));
+    strlcat(output_string, ". ", sizeof(output_string));
+  }
+  else
+  {
+    const int negative_range = get_neutral_speed()-get_min_speed();
+    int scaled_speed = ((get_neutral_speed()-speed)*100)/negative_range;
+
+    strlcpy(output_string, this->name, sizeof(output_string));
+    strlcat(output_string, " speed -", sizeof(output_string));
+    itoa(scaled_speed, int_string, 10);
+    strlcat(output_string, int_string, sizeof(output_string));
+    strlcat(output_string, ". Raw speed ", sizeof(output_string));
+    itoa(speed, int_string, 10);
+    strlcat(output_string, int_string, sizeof(output_string));
+    strlcat(output_string, ". ", sizeof(output_string));
+   }
+  
+  Serial.println(output_string);
+}
+
+sl_cr_motor_driver_virtual_c::sl_cr_motor_driver_virtual_c(const char* name, sl_cr_failsafe_f failsafe)
+  : sl_cr_motor_driver_c(failsafe)
+{
+  char output_string[SL_CR_MOTOR_DRIVER_VIRTUAL_MAX_CHARS] = {0};
+
+  active = false;
+
+  memset(this->name, '\0',  sizeof(this->name));
+  strlcpy(this->name, name, sizeof(this->name));
+
+  strlcpy(output_string, this->name, sizeof(output_string));
+  strlcat(output_string, " initialized.", sizeof(output_string));
+  Serial.println(output_string);
+}
---
+++ sl_cr_motor_driver_virtual.hpp
@@ -0,0 +1,23 @@
+#ifndef __SL_CR_MOTOR_DRIVER_VIRTUAL_HPP__
+#define __SL_CR_MOTOR_DRIVER_VIRTUAL_HPP__
+
+#include "sl_cr_motor_driver.hpp"
+
+#define SL_CR_MOTOR_DRIVER_VIRTUAL_MAX_CHARS 1024
+
+class sl_cr_motor_driver_virtual_c : sl_cr_motor_driver_c
+{
+  private:
+    /* Name of this virtual motor */
+    char name[SL_CR_MOTOR_DRIVER_VIRTUAL_MAX_CHARS];
+    /* Tracks if this motor is active */
+    bool active;
+
+    void disable_motor();
+    void command_motor();
+
+  public:
+    sl_cr_motor_driver_virtual_c(const char*, sl_cr_failsafe_f);
+};
+
+#endif /* __SL_CR_MOTOR_DRIVER_VIRTUAL_HPP__ */
